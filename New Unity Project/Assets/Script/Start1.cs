5b7a6503f36c4160f964cdc30c0235c45cb1a34f
陳宛昀 <lov011052@gmail.com>
2020-06-24 23:29:44 +0800

    0624fin

---
+++ New Unity Project/Assets/Script/Start1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start1 : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Application.LoadLevel("Meau");
+    }
+
+  
+}
---
+++ New Unity Project/Assets/Script/StartAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAudio : MonoBehaviour
+{
+    public AudioSource BingeAudio;
+   
+
+    // Update is called once per frame
+    void Start()
+    {
+        BingeAudio.Play();
+    }
+}
